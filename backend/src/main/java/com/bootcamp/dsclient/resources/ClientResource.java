1e3226da79b9e60359799205bac4ccb996f82eeb
Clovis Picoli <clovispicoli@gmail.com>
2020-10-08 21:16:34 -0300

    DTO

--- backend/src/main/java/com/bootcamp/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/bootcamp/dsclient/resources/ClientResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bootcamp.dsclient.entities.Client;
+import com.bootcamp.dsclient.dto.ClientDTO;
 import com.bootcamp.dsclient.services.ClientService;
 
 
@@ -20,8 +20,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/bootcamp/dsclient/services/ClientService.java
+++ backend/src/main/java/com/bootcamp/dsclient/services/ClientService.java
@@ -1,13 +1,16 @@
 package com.bootcamp.dsclient.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bootcamp.dsclient.dto.ClientDTO;
 import com.bootcamp.dsclient.entities.Client;
 import com.bootcamp.dsclient.repositories.ClientRepository;
+
 
 @Service
 public class ClientService {
@@ -16,9 +19,9 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());	
 	}
-	
-	
 }
